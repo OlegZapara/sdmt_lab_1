9405a5908a4d6ae109d9a0416f0123a33bb8a8d0
Chyhryn Vitalii <chyhryn.vitalii@gmail.com>
2025-03-13 23:17:33 +0200

    Implement interactive mode
    - Added get_coefficient function to read and parse user input for
      coefficients

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use core::f64;
+use std::{error, io};
+
 fn get_discriminant(a: f64, b: f64, c: f64) -> f64 {
     b.powi(2) - 4.0 * a * c
 }
@@ -15,4 +18,51 @@ fn get_quadratic_equation_roots(a: f64, b: f64, c: f64) -> (Option<f64>, Option<
     }
 }
 
-fn main() {}
+fn get_coefficient(name: &str) -> Result<f64, Box<dyn error::Error>> {
+    println!("Enter coefficient {name}: ");
+
+    let mut a = String::new();
+    io::stdin().read_line(&mut a)?;
+
+    let a: f64 = a.trim().parse()?;
+
+    Ok(a)
+}
+
+fn main() {
+    let a: f64 = loop {
+        match get_coefficient("a") {
+            Ok(val) => break val,
+            Err(err) => {
+                println!("Error: {err}")
+            }
+        }
+    };
+
+    let b: f64 = loop {
+        match get_coefficient("b") {
+            Ok(val) => break val,
+            Err(err) => {
+                println!("Error: {err}")
+            }
+        }
+    };
+
+    let c: f64 = loop {
+        match get_coefficient("c") {
+            Ok(val) => break val,
+            Err(err) => {
+                println!("Error: {err}")
+            }
+        }
+    };
+
+    println!("Equation: ({a}) * x ^ 2 + ({b}) * x + ({c})");
+
+    match get_quadratic_equation_roots(a, b, c) {
+        (None, None) => println!("There are no real roots"),
+        (Some(x), None) => println!("There is one real root: {x}"),
+        (Some(x1), Some(x2)) => println!("There are two real roots: {x1} and {x2}"),
+        _ => unreachable!("Unexpected root combination"),
+    }
+}
